2c9e55dced6ede95a1eb6dc03f19d846443a816c
ubuntu <ubuntu@localhost.localdomain>
2019-11-07 16:23:48 +0800

    xxx

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 g++ -c ./common/ThreadPool.cpp
-g++ -o server_exe -O3 server/server.cpp common/MyMd5.h common/netPack.h ThreadPool.o -lpthread -l crypto
-g++ -o client_exe -O3 ./client/client.cpp ./common/MyMd5.h -l crypto -lpthread
+g++ -o server_exe -O3 ./server/server.cpp ThreadPool.o -lpthread -l crypto
+g++ -o client_exe -O3 ./client/client.cpp -l crypto -lpthread
 rm ThreadPool.o
 mv *exe bin
